d8dd4e515bd4fed38f56cb864e21f6ec26993a5f
se-vahmoha-01 <se-vahmoha-01@SE-DEVPC1028>
2017-07-16 22:34:46 +0200

    add structuremap and autofac as container

--- Reciever/Program.cs
+++ Reciever/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Data;
 using System.Data.Odbc;
 using System.Data.SqlClient;
@@ -14,6 +13,7 @@ using NServiceBus.Persistence;
 using NServiceBus.Persistence.Sql;
 using Reciever;
 using Shared;
+using StructureMap;
 using Configuration = NHibernate.Cfg.Configuration;
 
 class Program
@@ -62,12 +62,30 @@ class Program
         endpointConfiguration.AuditProcessedMessagesTo("audit");
         endpointConfiguration.EnableInstallers();
 
+        //Ninject as Container
         var kernel = new StandardKernel();
         endpointConfiguration.UseContainer<NinjectBuilder>(
         customizations: customizations =>
         {
             customizations.ExistingKernel(kernel);
         });
+
+        //Autofac as container
+        //var builder = new ContainerBuilder();
+        //var container = builder.Build();
+        //endpointConfiguration.UseContainer<AutofacBuilder>(
+        //    customizations: customizations =>
+        //    {
+        //        customizations.ExistingLifetimeScope(container);
+        //    });
+
+        ///StructureMap as container
+        //var container = new Container();
+        //endpointConfiguration.UseContainer<StructureMapBuilder>(
+        //    customizations: customizations =>
+        //    {
+        //        customizations.ExistingContainer(container);
+        //    });
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration)
             .ConfigureAwait(false);
